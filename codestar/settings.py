406aacab2540e43032839fe127c6dc1911acbceb
Kasem Al <xdow69x0@students.codeinstitute.net>
2025-01-23 16:07:44 +0000

    add templates2

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-kasemdeautsc-djangoblog-7ocr5iu3sqd.ws.codeinstitute-ide.net',
