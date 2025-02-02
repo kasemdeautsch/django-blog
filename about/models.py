8ecad0728d804f2e4ef993e8d1175d9de6419a54
Kasem Al <xdow69x0@students.codeinstitute.net>
2025-02-02 07:07:55 +0000

    add imafe fild to about page

--- about/models.py
+++ about/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+from cloudinary.models import CloudinaryField
 # Create your models here.
 
 
@@ -7,6 +9,7 @@ class About(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
+    profile_image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return f'{self.title}.'
